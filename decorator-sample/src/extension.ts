1e1e3cb77e7d759bca7403288be8f8e347488873
Sébastien Levy <sebastien.levy@cdbdx.biz>
2021-01-21 12:52:59 +0100

    actualisation
    - ensure everything work with modern versions of vscode

--- decorator-sample/src/extension.ts
+++ decorator-sample/src/extension.ts
@@ -11,15 +11,13 @@ export function activate(context: vscode.ExtensionContext) {
 	const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
 		borderWidth: '1px',
 		borderStyle: 'solid',
-		overviewRulerColor: 'blue',
-		overviewRulerLane: vscode.OverviewRulerLane.Right,
 		light: {
 			// this color will be used in light color themes
-			borderColor: 'darkblue'
+			borderColor: 'darkred'
 		},
 		dark: {
 			// this color will be used in dark color themes
-			borderColor: 'lightblue'
+			borderColor: '#ff000055'
 		},
 		before: {
 			contentText: "->"
